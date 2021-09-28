3b53eb26b92c5131e3144d96668f2e00dc25dc22
Kyle <josiekausl266@gmail.com>
2021-09-28 17:12:47 +0800

    Command handler optimize

--- src/handler/command_handler.js
+++ src/handler/command_handler.js
@@ -3,17 +3,15 @@
 module.exports = (client, Discord) => {
     console.log('⚙️' + '  Loading Commands...');
     console.log('⚙️' + '  Loading Events...');
-    readCommands('./src/commands/codewars/', '../commands/codewars/', client);
-    readCommands('./src/commands/music/', '../commands/music/', client);
-    // I really need to learn about async, synchronous programming technique.
+    
+    fs.readdirSync('./src/commands').forEach(dirs => {
+        const commands = fs.readdirSync(`./src/commands/${dirs}`);
+
+        for(const file of commands) {
+            const command = require(`../commands/${dirs}/${file}`);
+            console.log(' ↳ Imported command ' + file);
+            client.commands.set(command.name, command);
+        }
+    });
 }
 
-function readCommands(path, requirePath, client) {
-    const command_files = fs.readdirSync(path).filter(file => file.endsWith('.js'));
-    for(file of command_files) {
-        const command = require(requirePath + file);
-        const file_name = file.match(/\w+/);
-        console.log(' ↳ Imported command ' + file_name[0]);
-        if(command.name) client.commands.set(command.name, command);
-    } 
-}
